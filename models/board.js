7d30f7401bbb8c0b8dc61f2c8a56f47ecb8799e9
sossw1 <sossw1@gmail.com>
2021-02-13 14:19:32 -0500

    Fixed linting

--- models/board.js
+++ models/board.js
@@ -1,24 +1,24 @@
 module.exports = (sequelize, DataTypes) => {
-    const Board = sequelize.define('Board', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            len: [1]
-        }
+  const Board = sequelize.define('Board', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1],
+    },
+  });
+
+  Board.associate = (models) => {
+    Board.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false,
+      },
     });
+    Board.hasMany(models.Task, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
 
-    Board.associate = (models) => {
-        Board.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-        Board.hasMany(models.Task, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    };
-
-    return Board;
+  return Board;
 };
